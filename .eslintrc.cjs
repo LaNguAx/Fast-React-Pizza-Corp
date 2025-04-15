937d4fc01fbd5d309e5d90d0ae1a621b91f889fa
LaNguAx <itayakni@gmail.com>
2025-04-15 17:06:11 +0300

    init folder structure

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    // ✅ This makes unused variables a warning instead of an error
+    "no-unused-vars": "warn",
+
+    // ✅ This disables the need for prop-types
+    "react/prop-types": "off",
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+export default function App() {
+  const x = 23;
+
+  return <div>Hello vite</div>;
+}
